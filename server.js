6f392b60c0b3df2b7b8e8ef2fcd9aa38e0a65496
Bryan Hughes <bryan@theoreticalideations.com>
2014-05-15 18:59:03 -0700

    Added some logging

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var five = require("johnny-five"),
     board = new five.Board();
 
+console.log('booting');
 board.on("ready", function() {
 
+  console.log('Booted, strobing');
   // Create an Led on pin 7 and strobe it on/off
   // Optionally set the speed; defaults to 100ms
   (new five.Led(7)).strobe();
