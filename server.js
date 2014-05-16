5ef300a49be778c5d53e1a575703c9667a3bce82
Bryan Hughes <bryan@theoreticalideations.com>
2014-05-15 19:25:00 -0700

    Running for realz

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-/*var five = require("johnny-five"),
-    board = new five.Board();*/
+var five = require("johnny-five"),
+    board = new five.Board();
 
-console.log('Running as ' + process.env.USER);
 console.log('booting');
-/*board.on("ready", function() {
+board.on('ready', function() {
 
   console.log('Booted, strobing');
   // Create an Led on pin 7 and strobe it on/off
   // Optionally set the speed; defaults to 100ms
   (new five.Led(7)).strobe();
 
-});*/
+});
